dee227b4065cbabedf4841b715e4ddb43f83785d
John Dill <dill.john.d@gmail.com>
2014-06-14 00:45:52 +0000

    loop exercises complete

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,20 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(str) {
+  var str = str.toLowerCase();
+  return str.split(',');
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(arr) {
+  var newArray = [];
+  for (var i = 0; i < arr.length; i++) {
+    newArray.push(i + ' is ' + arr[i]);
+  }
+  return newArray;
+};
