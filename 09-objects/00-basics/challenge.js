e0f3c2815a22ebf74dbe734938f46a20066b18da
John Dill <dill.john.d@gmail.com>
2014-06-14 00:09:41 +0000

    object exercieses complete

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,30 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(title, duration, students) {
+  var course = {};
+  course.title = title;
+  course.duration = duration;
+  course.students = students;
+  return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newVal) {
+  if (object.hasOwnProperty(newProp)) {
+    return object;
+  } else {
+    object[newProp] = newVal;
+    return object;
+  }
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+  return 'Hello ' + letter.recipient + ',\n\n' + letter.msg + '\n\nSincerely,\n' + letter.sender;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  var products = {};
+  products['MacBook Air'] = 999;
+  products['MacBook Pro'] = 1299;
+  products['Mac Pro'] = 2499;
+  products['Apple Sticker'] = 1;
+
+  return money >= products[item];
+};
